f3a87bf7ab77e5e1211505a1dd1a1d8a270e4282
Gagandeep Singh <87493848+gagan10071998@users.noreply.github.com>
2021-07-20 20:47:48 +0530

    update selection sort

--- Sorting Algorithms/1.selectionSort.cpp
+++ Sorting Algorithms/1.selectionSort.cpp
@@ -9,38 +9,46 @@ Selection Sort
 Selection Sort	Best -> Ω(n^2)	Average -> θ(n^2)	Worst -> O(n^2)
 
 *******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
 
+void swap(int arr[], int indexOne, int indexTwo){
+           int temp = arr[indexOne];
+           arr[indexOne] = arr[indexTwo];
+           arr[indexTwo] = temp;
+           return ;
+}
+
+void selectionSort(int arr[],int size){
+   if(size <= 1){
+       return;
+   }
+   else if(size==2){
+       if(arr[1]<arr[0]){
+           swap(arr,1,0);
+           return ;
+       }
+       return;
+   }
+   for(int startIndex = 0; startIndex<(size-1); startIndex++){
+       for(int iterator = (startIndex+1);iterator<size;iterator++){
+           if(arr[iterator] < arr[startIndex]){
+                swap(arr,iterator,startIndex);
+           }
+       }
+   }
+   return;
+}
+
+
 int main()
 {
-    int size;
-    cin >> size;
-    int unsortedArray[size];
-    
-    for(int x = 0; x<size; x++){
-        cin >> unsortedArray[x];
+    int a[] = {10,2,11,3,5,7,54,123,23,9,24,45};
+    int size = sizeof(a)/sizeof(a[0]);
+    selectionSort(a,size);
+    for(int i = 0; i < size; i++){
+        cout << a[i] << " ";
     }
-    
-    for(int x = 0; x<size; x++){
-        for(int y = (x+1); y<size; y++){
-            if(unsortedArray[x] > unsortedArray[y]){ // Swapping
-                int LowestElement = unsortedArray[x];
-                unsortedArray[x] = unsortedArray[y];
-                unsortedArray[y] = LowestElement;
-                continue;
-            }
-            else{
-                continue;
-            }
-        }
-    }
-    
-    for(int x = 0; x<size; x++){
-        cout << unsortedArray[x] << " ";
-    }
-    
     return 0;
 }
