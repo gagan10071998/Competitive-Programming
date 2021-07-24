b5d990eed8945359ce1eb4e126f93c50ba56361f
Gagandeep Singh <87493848+gagan10071998@users.noreply.github.com>
2021-07-24 22:57:51 +0530

    update merge sort

--- Sorting Algorithms/6.mergeSort.cpp
+++ Sorting Algorithms/6.mergeSort.cpp
@@ -11,88 +11,67 @@ Recursive Merge Sort    Best -> Ω(n log(n))	 Average -> θ(n log(n))	Worst -> O
 
 using namespace std;
 
-void merge(int unsortedArray[], int start, int mid, int end)
-{
-
-    int i = start;
-    int j = mid + 1;
-    int k = end;
-
-    int arr[(end - start) + 1];
-    int indexer = 0;
-    while (i <= mid && j <= end)
-    {
-        if (unsortedArray[i] >= unsortedArray[j])
-        {
-            arr[indexer] = unsortedArray[j];
-            indexer++;
-            j++;
-        }
-        else
-        {
-            arr[indexer] = unsortedArray[i];
-            indexer++;
+void merge(int arr[], int l, int mid, int r){
+    int arr2[r+1-l];
+    int start = l;
+    int i = 0;
+    int middel = mid+1;
+    while(l<=mid && middel<=r){
+        if(arr[l]>arr[middel]){
+            arr2[i] = arr[middel];
             i++;
+            middel++;
         }
-    }
-
-    if (i > mid)
-    {
-        while (j <= end)
-        {
-            arr[indexer] = unsortedArray[j];
-            indexer++;
-            j++;
-        }
-    }
-    else
-    {
-        while (i <= mid)
-        {
-            arr[indexer] = unsortedArray[i];
-            indexer++;
+        else{
+            arr2[i] = arr[l];
             i++;
+            l++;
         }
     }
-    indexer = 0;
-    for (int x = start; x <= end; x++)
-    {
-        unsortedArray[x] = arr[indexer];
-        indexer++;
+    
+    if(l<=mid){
+        while(l<=mid){
+            arr2[i] = arr[l];
+            i++;
+            l++;
+        }
     }
+    if(middel<=r){
+        while(middel<=r){
+            arr2[i] = arr[middel];
+            i++;
+            middel++;
+        }
+    }
+    l = start;
+    i = 0;
+    for(;l<=r;l++){
+        arr[l] = arr2[i];
+        i++;
+    }
+    return;
 }
 
-void mergeSort(int unsortedArray[], int start, int end)
-{
-    if (start == end)
-    {
-        return;
+void mergeSort(int arr[],int l, int r){
+    if(l<r){
+        int mid = (l+r)/2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l, mid, r);
     }
-
-    int half = (start + end) / 2;
-    mergeSort(unsortedArray, start, half);
-    mergeSort(unsortedArray, half + 1, end);
-
-    merge(unsortedArray, start, half, end);
+    
+    return;
 }
 
 int main()
 {
-    int size;
-    cin >> size;
-    int unsortedArray[size];
-
-    for (int x = 0; x < size; x++)
-    {
-        cin >> unsortedArray[x];
-    }
-
-    mergeSort(unsortedArray, 0, size - 1);
-
-    for (int x = 0; x < size; x++)
-    {
-        cout << unsortedArray[x] << " ";
-    }
-
-    return 0;
+   int arr[] = {23,56,23,45,23,54,7,34,2,1,34,6,657,34,23,2,345,546,56,34};
+   int size = sizeof(arr)/sizeof(arr[0]);
+   
+   mergeSort(arr,0,size-1);
+   
+   
+   for(int i = 0; i<size; i++){
+       cout<< arr[i] << " ";
+   }
 }
