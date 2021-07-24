502e019cb2ca617a0c7ff2316b80dd6b36262798
Gagandeep Singh <87493848+gagan10071998@users.noreply.github.com>
2021-07-24 18:56:01 +0530

    update recursive insertion sort

--- Sorting Algorithms/5.recursiveInsertionSort.cpp
+++ Sorting Algorithms/5.recursiveInsertionSort.cpp
@@ -5,55 +5,43 @@ Recursive Insertion Sort
 
 Recursive Insertion Sort	Best -> Ω(n) Average -> θ(n^2)	Worst -> O(n^2)
 *******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
 
-void insertionSort(int unsortedArray[], int size)
-{
 
-    if (size <= 1)
-    {
+void insertionSort(int arr[],int size){
+    
+    if(size<=1){
         return;
     }
-
-    insertionSort(unsortedArray, size - 1);
-
-    int LowestElement = unsortedArray[size - 1];
-    int indexer = size - 1;
-    for (int y = (size - 2); y >= 0; y--)
-    {
-        if (unsortedArray[y] > LowestElement)
-        {
-            unsortedArray[indexer] = unsortedArray[y];
-            unsortedArray[y] = LowestElement;
-            indexer = y;
+    insertionSort(arr, size-1);
+    int i = (size-1);
+    int temp = arr[i];
+    int swapIndex = i;
+    for(int j = (i-1); j>=0; j--){
+        if(arr[j]>temp){
+                arr[j+1] = arr[j];
+                swapIndex = j;
         }
-        else
-        {
-            continue;
+        else{
+            break;
         }
     }
+    arr[swapIndex] = temp;
+    
+    
 }
 
 int main()
 {
-    int size;
-    cin >> size;
-    int unsortedArray[size];
-
-    for (int x = 0; x < size; x++)
-    {
-        cin >> unsortedArray[x];
-    }
-
-    insertionSort(unsortedArray, size);
-
-    for (int x = 0; x < size; x++)
-    {
-        cout << unsortedArray[x] << " ";
-    }
-
-    return 0;
+   int arr[] = {23,56,23,45,23,54,7,34,2,1,34,6,657,34,23,2,345,546,56,34};
+   int size = sizeof(arr)/sizeof(arr[0]);
+   
+   insertionSort(arr,size);
+   
+   
+   for(int i = 0; i<size; i++){
+       cout<< arr[i] << " ";
+   }
 }
